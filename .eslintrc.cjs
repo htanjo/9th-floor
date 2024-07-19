74284984ef0d12edcdb10b5239c2090ff159e2df
Hiroyuki Tanjo <expheno@gmail.com>
2024-07-19 21:47:56 +0900

    Install eslint-config-airbnb

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,21 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:react/jsx-runtime',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.app.json',
+  },
   plugins: ['react-refresh'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
