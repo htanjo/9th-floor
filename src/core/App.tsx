ecf4e4b2fcfe17e01c2ddcaea0b06c3379f49be9
Hiroyuki Tanjo <expheno@gmail.com>
2024-11-05 16:22:07 +0900

    Code spliting

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,11 +1,16 @@
-import Screen from './Screen';
+import { lazy, Suspense } from 'react';
+import SplashScreen from '../ui/SplashScreen';
 import classes from './App.module.scss';
+
+const Screen = lazy(() => import('./Screen'));
 
 function App() {
   return (
     <div className={classes.app}>
       <div className={classes.container}>
-        <Screen />
+        <Suspense fallback={<SplashScreen />}>
+          <Screen />
+        </Suspense>
       </div>
     </div>
   );
