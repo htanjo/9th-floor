d339102964d5879c42f3e267baa056ebf3f2fe3a
Hiroyuki Tanjo <expheno@gmail.com>
2024-11-11 21:50:06 +0900

    Use low resolution textures for non-desktop devices

--- src/settings/assets.ts
+++ src/settings/assets.ts
@@ -1,18 +1,30 @@
-import mansionMeshUrl from '../assets/mansion.glb?url';
-import lightmap01TextureUrl from '../assets/lm_01_0001_rgbd.webp?url';
-import lightmap02TextureUrl from '../assets/lm_02_0001_rgbd.webp?url';
-import lightmap03TextureUrl from '../assets/lm_03_0001_rgbd.webp?url';
-import lightmap04TextureUrl from '../assets/lm_04_0001_rgbd.webp?url';
-import lightmap05TextureUrl from '../assets/lm_05_0001_rgbd.webp?url';
-import lightmap06TextureUrl from '../assets/lm_06_0001_rgbd.webp?url';
-import lightmap07TextureUrl from '../assets/lm_07_0001_rgbd.webp?url';
-import lightmap08TextureUrl from '../assets/lm_08_0001_rgbd.webp?url';
-import lightmap09TextureUrl from '../assets/lm_09_0001_rgbd.webp?url';
-import lightmap10TextureUrl from '../assets/lm_10_0001_rgbd.webp?url';
+import mansionHighMeshUrl from '../assets/mansion_high.glb?url';
+import mansionLowMeshUrl from '../assets/mansion_low.glb?url';
+import lightmap01HighTextureUrl from '../assets/lm_01_0001_rgbd_high.webp?url';
+import lightmap02HighTextureUrl from '../assets/lm_02_0001_rgbd_high.webp?url';
+import lightmap03HighTextureUrl from '../assets/lm_03_0001_rgbd_high.webp?url';
+import lightmap04HighTextureUrl from '../assets/lm_04_0001_rgbd_high.webp?url';
+import lightmap05HighTextureUrl from '../assets/lm_05_0001_rgbd_high.webp?url';
+import lightmap06HighTextureUrl from '../assets/lm_06_0001_rgbd_high.webp?url';
+import lightmap07HighTextureUrl from '../assets/lm_07_0001_rgbd_high.webp?url';
+import lightmap08HighTextureUrl from '../assets/lm_08_0001_rgbd_high.webp?url';
+import lightmap09HighTextureUrl from '../assets/lm_09_0001_rgbd_high.webp?url';
+import lightmap10HighTextureUrl from '../assets/lm_10_0001_rgbd_high.webp?url';
+import lightmap01LowTextureUrl from '../assets/lm_01_0001_rgbd_low.webp?url';
+import lightmap02LowTextureUrl from '../assets/lm_02_0001_rgbd_low.webp?url';
+import lightmap03LowTextureUrl from '../assets/lm_03_0001_rgbd_low.webp?url';
+import lightmap04LowTextureUrl from '../assets/lm_04_0001_rgbd_low.webp?url';
+import lightmap05LowTextureUrl from '../assets/lm_05_0001_rgbd_low.webp?url';
+import lightmap06LowTextureUrl from '../assets/lm_06_0001_rgbd_low.webp?url';
+import lightmap07LowTextureUrl from '../assets/lm_07_0001_rgbd_low.webp?url';
+import lightmap08LowTextureUrl from '../assets/lm_08_0001_rgbd_low.webp?url';
+import lightmap09LowTextureUrl from '../assets/lm_09_0001_rgbd_low.webp?url';
+import lightmap10LowTextureUrl from '../assets/lm_10_0001_rgbd_low.webp?url';
 import environmentOutdoorTextureUrl from '../assets/environment_outdoor.env?url';
 import environmentMirrorTextureUrl from '../assets/environment_mirror.env?url';
 import particleDustTextureUrl from '../assets/particle_dust.webp?url';
 import particleFogTextureUrl from '../assets/particle_fog.webp?url';
+import { isDesktop } from './general';
 
 export interface AssetConfig {
   type: 'mesh' | 'texture' | 'cubeTexture';
@@ -26,57 +38,57 @@ export const assetConfigs: AssetConfigs = [
   {
     type: 'mesh',
     name: 'mansion_mesh',
-    url: mansionMeshUrl,
+    url: isDesktop ? mansionHighMeshUrl : mansionLowMeshUrl,
   },
   {
     type: 'texture',
     name: 'lightmap_01_texture',
-    url: lightmap01TextureUrl,
+    url: isDesktop ? lightmap01HighTextureUrl : lightmap01LowTextureUrl,
   },
   {
     type: 'texture',
     name: 'lightmap_02_texture',
-    url: lightmap02TextureUrl,
+    url: isDesktop ? lightmap02HighTextureUrl : lightmap02LowTextureUrl,
   },
   {
     type: 'texture',
     name: 'lightmap_03_texture',
-    url: lightmap03TextureUrl,
+    url: isDesktop ? lightmap03HighTextureUrl : lightmap03LowTextureUrl,
   },
   {
     type: 'texture',
     name: 'lightmap_04_texture',
-    url: lightmap04TextureUrl,
+    url: isDesktop ? lightmap04HighTextureUrl : lightmap04LowTextureUrl,
   },
   {
     type: 'texture',
     name: 'lightmap_05_texture',
-    url: lightmap05TextureUrl,
+    url: isDesktop ? lightmap05HighTextureUrl : lightmap05LowTextureUrl,
   },
   {
     type: 'texture',
     name: 'lightmap_06_texture',
-    url: lightmap06TextureUrl,
+    url: isDesktop ? lightmap06HighTextureUrl : lightmap06LowTextureUrl,
   },
   {
     type: 'texture',
     name: 'lightmap_07_texture',
-    url: lightmap07TextureUrl,
+    url: isDesktop ? lightmap07HighTextureUrl : lightmap07LowTextureUrl,
   },
   {
     type: 'texture',
     name: 'lightmap_08_texture',
-    url: lightmap08TextureUrl,
+    url: isDesktop ? lightmap08HighTextureUrl : lightmap08LowTextureUrl,
   },
   {
     type: 'texture',
     name: 'lightmap_09_texture',
-    url: lightmap09TextureUrl,
+    url: isDesktop ? lightmap09HighTextureUrl : lightmap09LowTextureUrl,
   },
   {
     type: 'texture',
     name: 'lightmap_10_texture',
-    url: lightmap10TextureUrl,
+    url: isDesktop ? lightmap10HighTextureUrl : lightmap10LowTextureUrl,
   },
   {
     type: 'cubeTexture',
--- src/settings/general.ts
+++ src/settings/general.ts
@@ -1,6 +1,12 @@
+import Bowser from 'bowser';
+
 export const frameHeight = 200;
 export const hasPointingDevice = matchMedia('(pointer:fine)').matches; // TODO: May not be perfect.
 export const hasTouchscreen = matchMedia('(pointer:coarse)').matches;
 export function toRadians(degrees: number) {
   return degrees * (Math.PI / 180);
 }
+export const isDesktop = (() => {
+  const browser = Bowser.parse(window.navigator.userAgent);
+  return browser.platform.type === 'desktop';
+})();
