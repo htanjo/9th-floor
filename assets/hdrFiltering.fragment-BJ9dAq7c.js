6fc4ec8eee65bfbcc7a508b95494dee57c834476
Hiroyuki Tanjo <expheno@gmail.com>
2024-11-15 15:34:27 +0900

    Updates

--- assets/hdrFiltering.fragment-BJ9dAq7c.js
+++
@@ -1,5 +0,0 @@
-import{S as i}from"./Screen-5YHTTtu8.js";import"./hdrFilteringFunctions-Brye-ySk.js";import"./index-CuDbJH7e.js";const r="hdrFilteringPixelShader",e=`#include<helperFunctions>
-#include<importanceSampling>
-#include<pbrBRDFFunctions>
-#include<hdrFilteringFunctions>
-uniform float alphaG;uniform samplerCube inputTexture;uniform vec2 vFilteringInfo;uniform float hdrScale;varying vec3 direction;void main() {vec3 color=radiance(alphaG,inputTexture,direction,vFilteringInfo);gl_FragColor=vec4(color*hdrScale,1.0);}`;i.ShadersStore[r]=e;const l={name:r,shader:e};export{l as hdrFilteringPixelShader};
---
+++ assets/pass.fragment-CqnDJ0iK.js
@@ -0,0 +1,4 @@
+import{S as a}from"./Screen-G5rVbCAQ.js";import"./index--b2h7R6a.js";const e="passPixelShader",r=`varying vec2 vUV;uniform sampler2D textureSampler;
+#define CUSTOM_FRAGMENT_DEFINITIONS
+void main(void) 
+{gl_FragColor=texture2D(textureSampler,vUV);}`;a.ShadersStore[e]=r;const s={name:e,shader:r};export{s as passPixelShader};
