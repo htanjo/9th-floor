01c943b316c7a2e016f8b9b3ed0fbead9b27ab3e
Hiroyuki Tanjo <expheno@gmail.com>
2025-01-11 10:20:52 +0900

    Install Google Analytics

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,10 +1,23 @@
-import { lazy, Suspense } from 'react';
+import { lazy, Suspense, useEffect } from 'react';
+import ReactGA from 'react-ga4';
 import SplashScreen from '../ui/SplashScreen';
 import classes from './App.module.scss';
 
 const Screen = lazy(() => import('./Screen'));
 
 function App() {
+  useEffect(() => {
+    if (import.meta.env.PROD) {
+      ReactGA.initialize('G-MPT0YVV9GQ', {
+        gtagOptions: { send_page_view: false },
+      });
+      ReactGA.send({
+        hitType: 'pageview',
+        title: document.title,
+      });
+    }
+  }, []);
+
   return (
     <div className={classes.app}>
       <div className={classes.container}>
