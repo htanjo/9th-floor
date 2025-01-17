8e7202b67fdf65f9283a78cae5142b20109f3217
Hiroyuki Tanjo <expheno@gmail.com>
2025-01-17 22:54:51 +0900

    Updates

---
+++ assets/hdrFiltering.fragment-DRhbW2JE.js
@@ -0,0 +1,5 @@
+import{S as i}from"./Screen-NsoeUpAJ.js";import"./hdrFilteringFunctions-Cl8m9o49.js";import"./index-KGBvGren.js";const r="hdrFilteringPixelShader",e=`#include<helperFunctions>
+#include<importanceSampling>
+#include<pbrBRDFFunctions>
+#include<hdrFilteringFunctions>
+uniform float alphaG;uniform samplerCube inputTexture;uniform vec2 vFilteringInfo;uniform float hdrScale;varying vec3 direction;void main() {vec3 color=radiance(alphaG,inputTexture,direction,vFilteringInfo);gl_FragColor=vec4(color*hdrScale,1.0);}`;i.ShadersStore[r]=e;const l={name:r,shader:e};export{l as hdrFilteringPixelShader};
